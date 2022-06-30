1e5f448aa25ed0f620dc3e8417f5cd7ddc810301
Bruno Lisboa <brlisbo@gmail.com>
2022-06-30 20:59:09 -0300

    trabajando en la lista general de items

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,16 @@ function ItemList ( {product} ) {
   console.log( {product} );
 
   return (
-    <div> {product.title}</div >
+    <div className='container-itemlist'>
+      {/* <h1>Lista de Productos</h1>
+      <ul>
+        {
+          product.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))
+        }
+      </ul> */}
+    </div >
   ); 
 }
 
